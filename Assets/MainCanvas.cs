e01af57332df0656f6abdee0a1eb8689b4209c9b
Thomas the Franque <tfranque@gmail.com>
2020-01-17 01:08:21 +0000

    Prepare for better UI

---
+++ Assets/MainCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExternalSystemGames;
+
+public class MainCanvas : MonoBehaviour
+{
+    private List<GameButton> _buttons;
+
+    void Awake()
+    {
+        _buttons = new List<GameButton>(100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddGameButton(GameButton button)
+    {
+        _buttons.Add(button);
+    }
+}
